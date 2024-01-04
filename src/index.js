dc402bf16cb90e703d4dd013e50f9a2fa896d516
Elifelete-Cavalcante20 <elif.cav00@gmail.com>
2024-01-04 11:29:38 -0300

    Quarto commit (Final do Script)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,116 @@
 let NomeHeroi = "Mogli"
 let NivelHeroi = "0"
-let contador = "0"
-let Rank = "Ferro" 
-const Mensagem = "O herói de nome " + NomeHeroi + " está no nível de: " + Rank + " " 
+let Rank = "Ferro"
+const Espaco = " "
+const Subiu = "Ascendência de nível!!" 
+const Mensagem = "O herói de nome, " + NomeHeroi + ", está no nível: "
 
- for(contador = "0"; contador <= "10000"; contador ++){
-    NivelHeroi = contador;
+
+if (NivelHeroi = "0"){
+    console.log(Espaco)
+    console.log ("Inicio da jornada!!")
+    console.log("O herói de nome, " + NomeHeroi + ", ainda está sem nível!!")
+    console.log (NivelHeroi + " XP")
+}
+
+for (let i = "0"; i <= "1000"; i ++){
+    NivelHeroi = i;
 }
 
 if (NivelHeroi <= "1000"){
-    console.log(Mensagem)
-    console.log(NivelHeroi)
-} 
+    console.log(Espaco)
+    console.log(Subiu)
+    console.log(Mensagem + Rank)
+    console.log(NivelHeroi + " XP")
+}
+
+for (let q = "0"; q <= "2000"; q ++){
+    NivelHeroi = q;
+}
+
+ Rank = "Bronze"
+
+if (NivelHeroi >= "1001"){
+    console.log(Espaco)
+    console.log(Subiu)
+    console.log(Mensagem + Rank)
+    console.log(NivelHeroi + " XP")
+}
+
+for (let u = "0"; u <= "5000"; u ++){
+    NivelHeroi = u;
+}
+
+ Rank = "Prata"
+
+if (NivelHeroi >= "2001"){
+    console.log(Espaco)
+    console.log(Subiu)
+    console.log(Mensagem + Rank)
+    console.log(NivelHeroi + " XP")
+}
+
+for (let p = "0"; p <= "7000"; p ++){
+    NivelHeroi = p;
+}
+
+ Rank = "Ouro"
+
+if (NivelHeroi >= "5001"){
+    console.log(Espaco)
+    console.log(Subiu)
+    console.log(Mensagem + Rank)
+    console.log(NivelHeroi + " XP")
+}
+
+for (let s = "0"; s <= "8000"; s ++){
+    NivelHeroi = s;
+}
+
+ Rank = "Platina"
+
+if (NivelHeroi >= "7001"){
+    console.log(Espaco)
+    console.log(Subiu)
+    console.log(Mensagem + Rank)
+    console.log(NivelHeroi + " XP")
+}
+
+for (let a = "0"; a <= "9000"; a ++){
+    NivelHeroi = a;
+}
+
+ Rank = "Ascendente"
+
+if (NivelHeroi >= "8001"){
+    console.log(Espaco)
+    console.log(Subiu)
+    console.log(Mensagem + Rank)
+    console.log(NivelHeroi + " XP")
+}
+
+for (let t = "0"; t <= "10000"; t ++){
+    NivelHeroi = t;
+}
+
+ Rank = "Imortal"
+
+if (NivelHeroi >= "8001"){
+    console.log(Espaco)
+    console.log(Subiu)
+    console.log(Mensagem + Rank)
+    console.log(NivelHeroi + " XP")
+}
+
+for (let r = "0"; r <= "11000"; r ++){
+    NivelHeroi = r;
+}
+
+ Rank = "Radiante"
+
+if (NivelHeroi >= "8001"){
+    console.log(Espaco)
+    console.log(Subiu)
+    console.log(Mensagem + Rank)
+    console.log(NivelHeroi + " XP")
+}
